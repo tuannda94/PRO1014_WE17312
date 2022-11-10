5437cacfc8469bf6718e7206ec42c488c89c3252
Dotuananh1005 <105471609+Dotuananh1005@users.noreply.github.com>
2022-11-10 17:05:43 +0700

    B contrller product

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 
 // Trưởng nhóm bắt đầu dựng khung
 // Các thành viên sẽ kéo phần này về code tiếp
+
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
+// B tạo url sang Controller Product
